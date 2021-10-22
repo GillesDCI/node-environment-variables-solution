4ffce3cfaf2acffadbf074bffd07460dc6b47823
Gilles <gilles.vanhaecke-freelancer@digitalcareerinstitute.org>
2021-10-23 00:44:01 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Solution node-environment-variables 
+
+## Solution
+This is the solution to the node-environment-variables exercise. 
+
+*Note: there are many possible solutions, this is just an example of how it could be solved.*
---
+++ server.js
@@ -0,0 +1,21 @@
+//task 1
+// process.env.SERVER_NAME = 'Papagei 5000'
+// process.env.SERVER_CPU = 'Archimedes '
+// process.env.SERVER_URL = 'http://localhost:5000/'
+// process.env.SERVER_LOG = 'LOG.txt'
+
+//log the variables
+// console.log(process.env.SERVER_NAME) 
+// console.log(process.env.SERVER_CPU) 
+// console.log(process.env.SERVER_URL) 
+// console.log(process.env.SERVER_LOG) 
+
+require('dotenv').config();
+
+
+const {SERVER_NAME, SERVER_CPU, SERVER_URL, SERVER_LOG} = process.env
+
+console.log(`"Hallo! Ich bin ${SERVER_NAME}! Ich habe ein ${SERVER_CPU}, und mein URL ist ${SERVER_URL}. Ich schreibe alles was passiert zu ${SERVER_LOG}"`)
+
+
+
